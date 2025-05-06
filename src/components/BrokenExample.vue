5f603f6cb074d0ebec0a5b00c57499d2cb6dbe73
ben3345678 <q20038848@gmail.com>
2025-05-06 15:00:11 +0800

    fix: 修正 錯誤的測試檔案

--- src/components/BrokenExample.vue
+++ src/components/BrokenExample.vue
@@ -7,23 +7,14 @@
 <script>
 export default {
   name: 'BrokenExample',
-  props: {
-    msg: String,
-  },
   mounted() {
-    const example = 123
-    const badExample = 'change test123'
-    console.log(example)
-    console.log(badExample)
-    // const testFuc = () => {
-    //   console.log('test222', test2)
-    // }
-
-    function testFunc() {
-      console.log('testFunc', test2)
-    }
-    testFunc()
     const test2 = 'test2'
+    function testFunc(a, b) {
+      if (a === b) {
+        console.error('testFunc', test2)
+      }
+    }
+    testFunc('test', 'test')
   },
 }
 </script>
